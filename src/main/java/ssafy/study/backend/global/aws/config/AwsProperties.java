64b563ebcb0d1db2b4b6622d7f9a0a42f0784ec2
김해찬 <gibbm1127@naver.com>
2026-03-11 19:47:25 +0900

    feat(global): Add AWS S3 service and async/schedule infrastructure

--- src/main/java/ssafy/study/backend/global/aws/config/AwsProperties.java
+++ src/main/java/ssafy/study/backend/global/aws/config/AwsProperties.java
@@ -1,4 +1,12 @@
 package ssafy.study.backend.global.aws.config;
 
-public class AwsProperties {
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import jakarta.validation.constraints.NotBlank;
+
+@ConfigurationProperties(prefix = "custom.aws")
+public record AwsProperties(
+	@NotBlank String bucket,
+	@NotBlank String region
+) {
 }
--- src/main/java/ssafy/study/backend/global/aws/s3/S3Service.java
+++ src/main/java/ssafy/study/backend/global/aws/s3/S3Service.java
@@ -1,4 +1,64 @@
 package ssafy.study.backend.global.aws.s3;
 
+import java.time.Duration;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.S3Exception;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import ssafy.study.backend.global.aws.config.AwsProperties;
+import ssafy.study.backend.global.exception.CustomException;
+import ssafy.study.backend.global.exception.error.ErrorCode;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class S3Service {
+	private final S3Client s3Client;
+	private final S3Presigner s3Presigner;
+	private final AwsProperties awsProperties;
+
+	public String getDownloadPresignedUrl(String key) {
+		return s3Presigner.presignGetObject(b -> b
+			.signatureDuration(Duration.ofMinutes(10))
+			.getObjectRequest(g -> g.bucket(awsProperties.bucket()).key(key))
+		).url().toString();
+	}
+
+	public String getUploadPresignedUrl(String key, String contentType, Long contentLength) {
+
+		return s3Presigner.presignPutObject(b -> b
+			.signatureDuration(Duration.ofMinutes(5))
+			.putObjectRequest(p -> p
+				.bucket(awsProperties.bucket())
+				.key(key)
+				.contentType(contentType)
+				.contentLength(contentLength)
+			)
+		).url().toString();
+	}
+
+	public void deleteObject(String key) {
+		DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
+			.bucket(awsProperties.bucket())
+			.key(key)
+			.build();
+
+		try {
+			s3Client.deleteObject(deleteObjectRequest);
+		} catch(S3Exception e) {
+			log.error("AWS ERROR: s3 Obejct 삭제 에러가 발생하였습니다. AWS S3 Exception: {}", e.awsErrorDetails().errorMessage());
+			throw new CustomException(ErrorCode.INTERNAL_SERVER_ERROR);
+
+		} catch (Exception e) {
+			log.error("AWS ERROR: s3 Obejct 삭제 에러가 발생하였습니다.");
+			throw new CustomException(ErrorCode.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+
 }
