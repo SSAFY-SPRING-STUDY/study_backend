282649b5c56216b5737dae697a4ebb981d00b4b5
김해찬 <gibbm1127@naver.com>
2026-03-27 11:32:33 +0900

    hotfix: 공지사항 글자 수 제한 변경

--- src/main/java/ssafy/study/backend/domain/edu/notice/entity/Notice.java
+++ src/main/java/ssafy/study/backend/domain/edu/notice/entity/Notice.java
@@ -32,7 +32,7 @@ public class Notice {
 	@Column(nullable = false)
 	private String title;
 
-	@Column(nullable = false)
+	@Column(nullable = false, columnDefinition = "TEXT")
 	private String content;
 
 	@ManyToOne
